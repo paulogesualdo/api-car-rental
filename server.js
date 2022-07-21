66adb1e42ca44c702513ef090c64d4e2537465e0
“paulogesualdo” <paulo.gesualdo@outlook.com>
2022-07-21 12:27:28 -0300

    feat: adicionadas variaveis de ambiente

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const Hapi = require('@hapi/hapi');
+const environmentVariables = require('dotenv').config()
 
 module.exports = (async () => {
   const server = new Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        port: process.env.PORT,
+        host: process.env.HOST
     });
 
   return server;
