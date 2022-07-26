9eb5863c8dec9ee7fe7056a0e49dcdb3f8b97542
“paulogesualdo” <paulo.gesualdo@outlook.com>
2022-07-26 12:18:12 -0300

    chore: adicionado eslint

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const server = require('./server');
+const server = require("./server");
 const config = require("./config");
 
 const init = async () => {
-    try {
-      const serverInstance = await server;
+  try {
+    const serverInstance = await server;
   
-      await serverInstance.start();
-      console.log(`message: Server listening on port ${config.environmentVariables.PORT}, environment ${config.environmentVariables.NODE_ENV}`);
-    } catch (error) {
-      console.log(`message: App failed to start`);
-    }
-  };
+    await serverInstance.start();
+    console.log(`message: Server listening on port ${config.environmentVariables.PORT}, environment ${config.environmentVariables.NODE_ENV}`);
+  } catch (error) {
+    console.log("message: App failed to start");
+  }
+};
 
 init();
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const Hapi = require('@hapi/hapi');
-const config = require("./config")
+const Hapi = require("@hapi/hapi");
+const config = require("./config");
 
 module.exports = (async () => {
   const server = new Hapi.server({
-        port: config.environmentVariables.PORT,
-        host: config.environmentVariables.HOST
-    });
+    port: config.environmentVariables.PORT,
+    host: config.environmentVariables.HOST
+  });
 
   return server;
 })();
