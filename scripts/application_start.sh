c57560b7cc6409621ba4f00f11954b52bd83c4b8
Paulo Gesualdo <paulo.gesualdo@outlook.com>
2022-12-28 17:11:16 -0300

    retirado node index.js e corrigido final do arquivo

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,10 +8,10 @@ echo ">>>>>>>> APPLICATION START: NODE STOPPED"
 
 # start server
 cd /home/ec2-user/api-car-rental/
-node index.js
+#node index.js
 
 sudo systemctl enable api-car-rental.service
 sudo systemctl start api-car-rental.service
 echo ">>>>>>>> APPLICATION START: NODE STARTED"
 
-echo ">>>>>>>> APPLICATION START: 
+echo ">>>>>>>> APPLICATION START: END"
