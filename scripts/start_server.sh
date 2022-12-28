8a29c3b7f2a18b44c720fb57133329fdb3beee69
Paulo Gesualdo <paulo.gesualdo@outlook.com>
2022-12-28 12:34:10 -0300

    modificados arquivos para adequacao codedeploy

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
+
+# nvm install
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+echo ">>>>>>>> NVM INSTALLED"
+
+# use nvm without restarting the terminal
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+echo ">>>>>>>> NVM READY TO USE"
+
+# node install
+nvm install v14.17.0
+echo ">>>>>>>> NODE INSTALLED"
+
+# npm install
+# sudo nvm install-latest-npm
+# echo ">>>>>>>> NPM INSTALLED"
+
+rm -rf node_modules
+rm -f package-lock.json
+npm cache clean --force
+
+# node modules install
+npm install
+echo ">>>>>>>> NODE MODULES INSTALLED"
+
+cd /home/ec2-user/teste2/api-car-rental/
+
+# start server
 node index.js
